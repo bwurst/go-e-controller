fe5df64d4c8b53c5c550c66fb5ffc7898792503e
Bernd Wurst <bernd@bwurst.org>
2019-07-11 18:11:26 +0200

    fix type mismatch (could not detect end of charge) / reduce PV current by 1 to leave some electricity for the rest of the house

--- go-e.py
+++ go-e.py
@@ -7,7 +7,7 @@ import math
 import logging
 import os.path
 
-logging.basicConfig(filename=os.path.join(os.path.dirname(os.path.realpath(__file__)), 'go-e.log'),format='%(asctime)s %(levelname)s:%(message)s', level=logging.DEBUG)
+logging.basicConfig(filename=os.path.join(os.path.dirname(os.path.realpath(__file__)), 'go-e.log'),format='%(asctime)s %(levelname)s:%(message)s', level=logging.INFO)
 
 
 def get_pv_current():
@@ -40,7 +40,6 @@ class GOE(object):
         self.get_status()
 
     def __getitem__(self, var):
-        logging.debug('goe['+var+'] = '+self.status[var])
         return self.status[var]
 
     def __setitem__(self, var, val):
@@ -65,25 +64,31 @@ class GOE(object):
 try:
     goe = GOE()
 
+    if int(goe['amp']) != 16:
+        logging.info('car = %s / amp = %s' % (goe['car'], goe['amp']));
 
     # when charge is done and car still connected, always reset to 16A for the next time
     # if goe['car'] == 4: set amp=16
-    if goe['car'] == 4 and goe['amp'] != 16:
-        goe['amp'] = 16
-
+    if int(goe['car']) == 4 and int(goe['amp']) != 16:
+        logging.info('reset power to 16A')
+        goe['amp'] = '16'
 
     # when user selected 32A manually, don't touch. When 32A charging is over, current has been reset in the last step.
     # if goe['amp'] == 32: do nothing
     # our work is to be done when car is charging!
     # if goe['car'] == 2: run
-    if goe['amp'] != 32 and goe['car'] == 2:
+    if int(goe['amp']) != 32 and int(goe['car']) == 2:
         # when car charges
         # look for PV current
         # calc max ampere from PV output
         try:
             # thows error sometimes, then leave all alone
             pv = get_pv_current()
+            logging.debug('PV gains %s A' % pv)
+            pv = pv - 1
+            logging.info('reducing PV by 1 A, remaining charge current: %s A' % pv)
         except:
+            logging.debug('error reading PV power')
             # don't change
             pv = goe['amp']
 
@@ -91,14 +96,15 @@ try:
         # elif pv['amp'] > 20: goe['amp'] = 20
         # else: goe['amp'] = pv['amp']
         if pv < 6:
-            logging.debug('PV < 6 => 6')
+            logging.info('PV < 6 => 6')
             goe['amp'] = '6'
         elif pv > 20:
-            logging.debug('PV > 20 => 20')
+            logging.info('PV > 20 => 20')
             goe['amp'] = '20'
         else:
-            logging.debug('PV = %s => %s' % (pv, pv))
+            logging.info('PV = %s => %s' % (pv, pv))
             goe['amp'] = '%s' % pv
 except requests.exceptions.ConnectionError:
+    logging.error('exception in main program')
     pass
 
