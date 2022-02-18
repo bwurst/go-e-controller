f33aaa39dd6c68908ffb09409f1a06fa075331ca
Bernd Wurst <bernd@bwurst.org>
2022-02-18 17:59:49 +0100

    better phase switching algorithm, improved config

--- go-e.py
+++ go-e.py
@@ -19,8 +19,8 @@ with open(configfile, 'r') as conf:
 # config-example: [{'goe-address': 'http://goe', 'pvtype': 'SBFspot', 'sbfspotconfig': 'Haus32.cfg'}]
 
 
-logging.basicConfig(filename=os.path.join(os.path.dirname(os.path.realpath(__file__)), 'go-e.log'),format='%(asctime)s %(levelname)s:%(message)s', level=logging.DEBUG)
-#logging.basicConfig(filename=os.path.join(os.path.dirname(os.path.realpath(__file__)), 'go-e.log'),format='%(asctime)s %(levelname)s:%(message)s', level=logging.INFO)
+#logging.basicConfig(filename=os.path.join(os.path.dirname(os.path.realpath(__file__)), 'go-e.log'),format='%(asctime)s %(levelname)s:%(message)s', level=logging.DEBUG)
+logging.basicConfig(filename=os.path.join(os.path.dirname(os.path.realpath(__file__)), 'go-e.log'),format='%(asctime)s %(levelname)s:%(message)s', level=logging.INFO)
 
 def get_sbfspot_current_wrapper(sbfspotconfig):
     def get_pv_current():
@@ -45,103 +45,153 @@ def get_sbfspot_current_wrapper(sbfspotconfig):
 
 
 class GOE(object):
-    status = {}
-
     def __init__(self, address):
+        self.status = {}
         self.address = address
-        self.get_status()
+        self.hostname = re.sub(r"https?://(?P<hostname>.*)/?$", r"\1", self.address)
+        try:
+            self.get_status()
+        except RuntimeError:
+            pass
 
     def __getitem__(self, var):
-        return self.status[var]
+        try:
+            return self.status[var]
+        except:
+            return None
 
     def __setitem__(self, var, val):
-        self.set_var(var, val)
+        try:
+            self.set_var(var, val)
+        except:
+            pass
 
     def get_status(self):
-        r = requests.get(self.address+'/api/status')
-        self.status = r.json()
-        logging.debug(str(self.status))
+        try:
+            r = requests.get(self.address+'/api/status', timeout=20)
+            self.status = r.json()
+        except (requests.exceptions.ChunkedEncodingError, requests.exceptions.RequestException) as e:
+            # connection problem
+            logging.error('exception while getting status from charger "%s": %s' % (self.hostname, str(e)))
+            raise RuntimeError('exception while getting status from charger "%s"' % self.hostname)
+        #logging.debug(str(self.status))
 
 
     def set_var(self, var, value):
         oldval = self.status[var]
-        logging.debug('old value: %s (type %s) / new value: %s (type: %s)' % (oldval, type(oldval), value, type(value)))
+        logging.debug('%s: %s: old value: %s (type %s) / new value: %s (type: %s)' % (self.hostname, var, oldval, type(oldval), value, type(value)))
         if value == oldval:
             return
-        logging.info('set %s from %s to %s' % (var, oldval, value))
+        logging.info('%s: set %s from %s to %s' % (self.hostname, var, oldval, value))
         payload='%s=%s' % (var, value)
-        r = requests.get(self.address+'/api/set?'+payload)
-        if r.text != '1 succeeded and 0 failed.':
-            logging.error('api call returned '+r.text)
-        self.get_status()
+        try:
+            r = requests.get(self.address+'/api/set?'+payload, timeout=20)
+            result = r.json()
+            if not result[var]:
+                logging.error('%s: api call returned '+r.text % self.hostname)
+            self.get_status()
+        except (requests.exceptions.ChunkedEncodingError, requests.exceptions.RequestException):
+            logging.error('%s: exception while setting %s from %s to %s' % (self.hostname, var, oldval, value))
+            pass
         if int(self.status[var]) != int(value):
-            raise RuntimeError('charger did not accept value change')
+            raise RuntimeError('charger "%s" did not accept value change' % self.hostname)
 
-def work(goe_address, pvcallback):
+def work(goe_address, pvcallback, config):
     try:
         goe = GOE(goe_address)
+        if not goe.status:
+            raise RuntimeError('error initializing goe object')
 
         phases = 3
         if int(goe['psm']) == 1:
             phases = 1
-        if int(goe['amp']) != 16 or phases != 3:
-            logging.info('car = %s / amp = %s / phases=%s' % (goe['car'], goe['amp'], phases));
+        used_phases = 0
+        for i in range(3):
+            if int(goe['nrg'][2]) > 0:
+                used_phases += 1
 
-        # when charge is done and car still connected, always reset to 16A for the next time
-        # if goe['car'] == 4: set amp=16
-        if int(goe['car']) == 4 and (int(goe['amp']) != 16 or phases != 3):
-            logging.info('reset power to 16A')
-            goe['amp'] = '16'
-            goe['psm'] = '2'
+        if int(goe['amp']) != 16 or phases != 3:
+            logging.info('%s: car = %s / amp = %s / phases=%s / frc=%s' % (goe.hostname, goe['car'], goe['amp'], phases, goe['frc']));
+
+        # when no car is connected, reset to 16A on three phases for the next time
+        if int(goe['car']) == 1:
+            if int(goe['amp']) != 16 or phases != 3:
+                logging.info('%s: reset power to 16A' % goe.hostname)
+                goe['amp'] = '16'
+                goe['psm'] = '2'
+            # do nothing else, as no car is connected
+            return
 
         # when user selected 32A manually, don't touch. When 32A charging is over, current has been reset in the last step.
-        # if goe['amp'] == 32: do nothing
-        # our work is to be done when car is charging!
-        # if goe['car'] == 2: run
-        if int(goe['amp']) != 32 and (int(goe['car']) == 2 or (len(sys.argv) > 1 and sys.argv[1] == '-f')):
-            # when car charges
-            # look for PV current
-            # calc max ampere from PV output
-            try:
-                # thows error sometimes, then leave all alone
-                pv = pvcallback()
-                if not pv:
-                    # it's night switch to 11kW charge, it does not matter
-                    logging.info("it's night, switch to 16A on three phases to get the car full")
-                    pv=11540
-                logging.info('PV gains %s W' % pv)
-                # house usage: ~500W
-                pv = max(pv - 500, 0)
-                pv_current = round((pv / 230) / 3)
-                logging.info('reducing PV by 500 Watts, remaining charge power: %s W / %s A' % (pv, pv_current))
-            except:
-                logging.error('error reading PV power')
-                # don't change
-                pv = int(goe['amp']) * 230 * 3
-                pv_current = int(goe['amp'])
+        if goe['amp'] == 32 and not (len(sys.argv) > 1 and sys.argv[1] == '-f'):
+            # do not touch
+            return
 
-            # if pv['amp'] < 6: goe['amp'] = 6
-            # else: goe['amp'] = pv['amp']
-            if phases == 1 and pv_current > 10:
-                # switch to three-phase-charging
-                logging.info('PV > 10 => switch to 3 phases')
-                goe['psm'] = 2
-                phases = 3
-            if pv_current < 10 and phases == 3:
-                # switch to one-phase-charging
-                logging.info('PV < 10 => switch to 1 phase')
-                goe['psm'] = 1
-                phases = 1
-            if phases == 1:
-                pv_current = round(pv / 230)
-            logging.info('loading on %i phase(s) with %s A, using %i W' % (phases, pv_current, pv_current * phases * 230))
-            if pv_current < 10:
-                logging.info('PV == %s (< 10) => 10' % (pv_current,))
-                goe['amp'] = 10
+        # when car charges
+        # look for PV current
+        # calc max ampere from PV output
+        power = 0
+        try:
+        # thows error sometimes, then leave all alone
+            power = pvcallback()
+            if not power:
+                logging.info("%s: it's night" % goe.hostname)
+                power = 0
             else:
-                logging.info('PV = %s => %s' % (pv_current, pv_current))
-                goe['amp'] = pv_current
+                logging.info('%s: PV gains %s W' % (goe.hostname, power))
+                # house usage: ~500W
+                consumption = 500
+                if "consumption" in config:
+                    consumption = config['consumption']
+                power = max(power - consumption, 0)
+                logging.info('%s: reducing PV by 500 Watts, remaining charge power: %s W' % (goe.hostname, power))
+        except Exception as e:
+            logging.error('%s: error reading PV power: %s' % (goe.hostname, str(e)))
+            # don't change
+            return
+
+        min_current = 10
+        if "min_current" in config:
+            min_current = config["min_current"]
+        max_1pha_current = 30
+        if "max_current" in config:
+            max_1pha_current = config["max_current"]
+        current = round((power / 230) / used_phases)
+
+        if phases == 1 and current < min_current:
+            # bisher schon auf einer phase und dafür zu wenig leistung...
+            logging.info('%s: power too low, stop charging.' % goe.hostname)
+            goe['frc'] = 1 #stop
+            return
+        else:
+            goe['frc'] = 0 #neutral
+
+        # Bleibt so lange auf einer Phase bis 30 Ampere (= Das Minimum von 10A auf 3 Phasen) überschritten werden
+        # Bleibe so lange auf 3 Phasen, bis das Minimum unterschritten wird.
+        if phases == 1 and current > max_1pha_current:
+            # switch to three-phase-charging
+            logging.info('%s: PV > %s A => switch to 3 phases' % (goe.hostname, max_1pha_current))
+            goe['psm'] = 2
+            phases = 3
+        if current < min_current and phases == 3 and used_phases > 1:
+            # switch to one-phase-charging
+            logging.info('%s: PV < %s A => switch to 1 phase' % (goe.hostname, min_current))
+            goe['psm'] = 1
+            phases = 1
+
+        current = round((power / 230) / min(used_phases, phases))
+        power = current * 230 * min(used_phases, phases)
+        if current < min_current:
+            logging.info('%s: PV power below minimum: %s A / %s W' % (goe.hostname, current, power))
+            current = min_current
+        power = current * 230 * min(used_phases, phases)
+        logging.info('%s: setting charger to %s A / %s W (phases: %s / used_phases: %s)' % (goe.hostname, current, power, phases, min(used_phases, phases)))
+        goe['amp'] = current
+
     except requests.exceptions.ConnectionError:
+        logging.error('exception in main program')
+        pass
+    except RuntimeError:
         logging.error('exception in main program')
         pass
 
@@ -149,5 +199,5 @@ for system in config:
     get_pv_current = None
     if system['pvtype'] == 'SBFspot':
         get_pv_current = get_sbfspot_current_wrapper(system['sbfspotconfig'])
-    work(system['goe_address'], get_pv_current)
+    work(system['goe_address'], get_pv_current, system)
 
